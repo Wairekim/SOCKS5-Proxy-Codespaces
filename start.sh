e6c21e30bb4d6e8d3320767c4518695298c055b3
Wairekim <wairekim@users.noreply.github.com>
2026-06-05 20:05:43 +0300

    Fix tinyproxy allow rule

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-sudo sed -i 's/^Port .*/Port 1080/' /etc/tinyproxy/tinyproxy.conf
-sudo sed -i '/^Allow /d' /etc/tinyproxy/tinyproxy.conf
+sudo sed -i 's/^[[:space:]]*Port .*/Port 1080/' /etc/tinyproxy/tinyproxy.conf
+sudo sed -i '/^[[:space:]]*Allow /d' /etc/tinyproxy/tinyproxy.conf
+sudo sed -i '/^[[:space:]]*Deny /d' /etc/tinyproxy/tinyproxy.conf
+sudo sed -i '/^[[:space:]]*ConnectPort /d' /etc/tinyproxy/tinyproxy.conf
 echo 'Allow 0.0.0.0/0' | sudo tee -a /etc/tinyproxy/tinyproxy.conf
+echo 'Allow ::/0' | sudo tee -a /etc/tinyproxy/tinyproxy.conf
 echo 'DisableViaHeader Yes' | sudo tee -a /etc/tinyproxy/tinyproxy.conf
 
 sudo tinyproxy
-
 echo "Tinyproxy started on port 1080"
